577b8c6c1ac499fb3cdab4898357735c1445ba8b
yellpall <ohj3423@naver.com>
2023-06-07 19:14:46 +0900

    component

---
+++ src/components/footer/index.js
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+export default function Footer() {
+    return <div>footer</div>
+}
---
+++ src/components/header/index.js
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+export default function Header() {
+    return <div className={styles.red}>header</div>
+}
